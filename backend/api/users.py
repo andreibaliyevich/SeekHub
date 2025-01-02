219648685e2d1268bc5aab688a2d498f33390106
Andrei Baliyevich <88473526+andreibaliyevich@users.noreply.github.com>
2025-01-03 02:46:59 +0300

    Add routers

---
+++ backend/api/users.py
@@ -0,0 +1,37 @@
+from typing import Annotated
+from uuid import UUID
+from fastapi import APIRouter, Form, Request
+from api.dependencies import UOWDep
+from schemas.users import UsersBase
+from services.users import UsersService
+
+
+router = APIRouter(
+    prefix="/users",
+    tags=["Users"],
+)
+
+
+@router.get("/list")
+async def user_list(request: Request, uow: UOWDep):
+    return await UsersService().get_queryset(uow, dict(request.query_params))
+
+
+@router.post("/add")
+async def add_user(data: Annotated[UsersBase, Form()], uow: UOWDep):
+    return await UsersService().add_user(uow, data)
+
+
+@router.get("/get/{id}")
+async def get_user(id: UUID, uow: UOWDep):
+    return await UsersService().get_user_by_id(uow, id)
+
+
+@router.put("/update/{id}")
+async def update_user(id: UUID, data: Annotated[UsersBase, Form()], uow: UOWDep):
+    return await UsersService().update_user(uow, id, data)
+
+
+@router.delete("/delete/{id}")
+async def add_user(id: UUID, uow: UOWDep):
+    return await UsersService().delete_user(uow, id)

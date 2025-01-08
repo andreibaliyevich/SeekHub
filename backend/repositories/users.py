a711246a082dea2e2367d3ed72e17afad2caf5d1
Andrei Baliyevich <88473526+andreibaliyevich@users.noreply.github.com>
2025-01-08 10:54:13 +0300

    Add obj_by_email method

--- backend/repositories/users.py
+++ backend/repositories/users.py
@@ -1,6 +1,13 @@
+from pydantic import EmailStr
+from sqlmodel import select
 from models.users import Users
 from utilities.repository import SQLRepository
 
 
 class UsersRepository(SQLRepository):
     model = Users
+
+    async def obj_by_email(self, email: EmailStr):
+        stmt = select(self.model).filter_by(email=email)
+        res = await self.session.execute(stmt)
+        return res.scalar_one_or_none()

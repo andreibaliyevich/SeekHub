f3ddecde002988f24a53ef55ef9b29e4f5219968
Andrei Baliyevich <88473526+andreibaliyevich@users.noreply.github.com>
2025-01-12 22:42:51 +0300

    Add PermissionDeniedError

--- backend/exceptions/auth.py
+++ backend/exceptions/auth.py
@@ -8,3 +8,12 @@ class InvalidCredentialsError(HTTPException):
             detail=detail,
             headers={"WWW-Authenticate": "Bearer"},
         )
+
+
+class PermissionDeniedError(HTTPException):
+    def __init__(self, detail: str = "No permission to this resource"):
+        super().__init__(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=detail,
+            headers={"X-Error-Reason": "Permission Denied"},
+        )

c50e3669abc5c23945bd354a6105574bce5ac401
Andrei Baliyevich <88473526+andreibaliyevich@users.noreply.github.com>
2025-01-25 15:13:57 +0300

    Updating transactions

--- backend/services/auth.py
+++ backend/services/auth.py
@@ -41,11 +41,15 @@ class AuthService:
         user_dict["hashed_password"] = get_password_hash(data.password)
         del user_dict["password"]
         del user_dict["confirm_password"]
-        new_user = await self.uow.users_repository.add(user_dict)
-        await self.uow.session.flush()
-        await self.uow.profiles_repository.add({"id": new_user.id})
-        await self.uow.commit()
-        await self.uow.session.refresh(new_user)
+        try:
+            async with self.uow.session.begin():
+                new_user = await self.uow.users_repository.add(user_dict)
+                await self.uow.session.flush()
+                await self.uow.profiles_repository.add({"id": new_user.id})
+                await self.uow.session.refresh(new_user)
+        except Exception as e:
+            await self.uow.session.rollback()
+            raise ValueError(f"Failed to register user and profile: {e}")
         return new_user
 
     async def change_password(self, user: Users, data: PasswordChange):
@@ -65,13 +69,39 @@ class AuthService:
     ):
         user_dict = body_data.model_dump(exclude_none=True)
         profile_dict = user_dict.pop("profile", {})
-        user = await self.uow.users_repository.update(user.id, user_dict)
-        profile = await self.uow.profiles_repository.update(user.id, profile_dict)
-        await self.uow.commit()
-        await self.uow.session.refresh(user)
-        await self.uow.session.refresh(profile)
-        user.profile = profile
-        return user
+
+        updated_user = None
+        updated_profile = None
+
+        if user_dict:
+            try:
+                async with self.uow.session.begin_nested():
+                    updated_user = await self.uow.users_repository.update(user.id, user_dict)
+                    await self.uow.session.flush()
+            except Exception as e:
+                print(f"Error updating user: {e}")
+
+        if profile_dict:
+            try:
+                async with self.uow.session.begin_nested():
+                    updated_profile = await self.uow.profiles_repository.update(user.id, profile_dict)
+                    await self.uow.session.flush()
+            except Exception as e:
+                print(f"Error updating profile: {e}")
+
+        if updated_user or updated_profile:
+            await self.uow.commit()
+
+        if updated_user:
+            await self.uow.session.refresh(updated_user)
+        else:
+            updated_user = user
+
+        if updated_profile:
+            await self.uow.session.refresh(updated_profile)
+
+        updated_user.profile = updated_profile
+        return updated_user
 
     async def delete_user_profile(self, user: Users):
         user_id = await self.uow.users_repository.delete(user.id)

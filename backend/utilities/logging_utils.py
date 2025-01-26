92956b8bf44fa4bd9970b3453db6f160ab3f3b37
Andrei Baliyevich <88473526+andreibaliyevich@users.noreply.github.com>
2025-01-26 10:38:36 +0300

    Add logging utility

---
+++ backend/utilities/logging_utils.py
@@ -0,0 +1,22 @@
+import logging
+
+
+logger = logging.getLogger("app")
+logger.setLevel(logging.INFO)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+console_handler.setFormatter(console_formatter)
+
+file_handler = logging.FileHandler("app.log", mode="a")
+file_handler.setLevel(logging.INFO)
+file_formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+file_handler.setFormatter(file_formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)

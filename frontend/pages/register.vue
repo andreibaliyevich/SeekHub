4f83add3919e91d45f9e35e1a4de54d8b46833da
Andrei Baliyevich <88473526+andreibaliyevich@users.noreply.github.com>
2025-02-03 21:28:31 +0300

    Create register.vue

---
+++ frontend/pages/register.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Register',
+  ogTitle: 'Register',
+  description: 'Register Seek Hub',
+  ogDescription: 'Register Seek Hub',
+})
+</script>
+
+<template>
+  <div style="margin-top: 64px;">
+    <h1>Welcome to the Register page</h1>
+  </div>
+</template>

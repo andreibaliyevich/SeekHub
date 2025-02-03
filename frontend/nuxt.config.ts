23933ada10901dcc8079db1d67483cd9abf427a0
Andrei Baliyevich <88473526+andreibaliyevich@users.noreply.github.com>
2025-02-03 19:44:32 +0300

    Add main css

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,6 +3,7 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  css: ['~/assets/main.css'],
   build: {
     transpile: ['vuetify']
   },

1a1ed8f95e095826f15f9ca37a36934974037535
Andrei Baliyevich <88473526+andreibaliyevich@users.noreply.github.com>
2025-02-03 21:30:17 +0300

    Update Translations

--- frontend/locales/en.ts
+++ frontend/locales/en.ts
@@ -1,7 +1,38 @@
 export default defineI18nLocale(async locale => {
   return {
+    header: {
+      about_us: 'About Us',
+      howItWorks: 'How it Works',
+      login: 'Log in',
+      register: 'Register'
+    },
+    footer: {
+      description: "SeekHub — an exclusive platform for ambitious and successful individuals who value quality in every connection.",
+      rules: "Rules",
+      advertising: "Advertising",
+      our_logos: "Our Logos",
+      feedback: "Feedback",
+      more_about_seekhub: "More about SeekHub",
+      analytics: "Analytics",
+      career_at_seekhub: "Career at SeekHub",
+      terms_of_use: "Terms of Use",
+      privacy_policy: "Privacy Policy",
+      advertising_and_promotion: "Advertising and promotion"
+    },
+    pages: {
+      home: {
+        title: 'Home',
+        description: 'Home Seek Hub',
+        keywords: 'Home, Seek, Hub'
+      },
+      about: {
+        title: 'About Us',
+        description: 'About Seek Hub',
+        keywords: 'About, Seek, Hub'
+      }
+    },
     hello: 'Hello, {name}!',
-    min_number_hours: "minimum 0 hours | minimum 1 hour | minimum {n} hours",
-    language: "Language"
+    min_number_hours: 'minimum 0 hours | minimum 1 hour | minimum {n} hours',
+    language: 'Language'
   }
 })
--- frontend/locales/ru.ts
+++ frontend/locales/ru.ts
@@ -1,7 +1,38 @@
 export default defineI18nLocale(async locale => {
   return {
+    header: {
+      about_us: 'О нас',
+      howItWorks: 'Как это работает',
+      login: 'Войти',
+      register: 'Зарегистрироваться'
+    },
+    footer: {
+      description: "SeekHub — эксклюзивная платформа для амбициозных и успешных людей, которые ценят качество во всем.",
+      rules: "Правила",
+      advertising: "Реклама",
+      our_logos: "Наши Логотипы",
+      feedback: "Обратная связь",
+      more_about_seekhub: "Подробнее о SeekHub",
+      analytics: "Аналитика",
+      career_at_seekhub: "Карьера в SeekHub",
+      terms_of_use: "Условия использования",
+      privacy_policy: "Политика конфиденциальности",
+      advertising_and_promotion: "Реклама и продвижение"
+    },
+    pages: {
+      home: {
+        title: 'Главная',
+        description: 'Главная Seek Hub',
+        keywords: 'Главная, Seek, Hub'
+      },
+      about: {
+        title: 'О нас',
+        description: 'О Seek Hub',
+        keywords: 'О, Seek, Hub'
+      }
+    },
     hello: 'Привет, {name}!',
-    min_number_hours: "минимум 0 часов | минимум 1 час | минимум {n} часа | минимум {n} часов",
-    language: "Язык"
+    min_number_hours: 'минимум 0 часов | минимум 1 час | минимум {n} часа | минимум {n} часов',
+    language: 'Язык'
   }
 })
